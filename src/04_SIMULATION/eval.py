c9d27487a91602991a9ac648c4d9aec5e3659a87
Ao Qu <aoqu@dhcp-10-31-44-210.dyn.MIT.EDU>
2022-10-12 13:58:42 -0400

    code for training and eval

---
+++ src/04_SIMULATION/eval.py
@@ -0,0 +1,35 @@
+from sb3_contrib.ppo_mask import MaskablePPO
+from run import BusEnv
+import numpy as np
+
+if __name__=="__main__":
+    config = {"HOLD_INTERVALS": 60,
+              "IMPOSED_DELAY_LIMIT": 240}
+    env = BusEnv(config)
+    env.reset()
+
+    model_path = "models/PPO/200000.zip"
+    model = MaskablePPO.load(model_path, env=env)
+
+    episodes = 1
+    all_rewards = []
+    reward_list = []
+    for ep in range(episodes):
+        obs = env.reset()
+        done = False
+        cnt = 0
+        rewards = []
+        while not done:
+            action, _ = model.predict(obs)
+            print("Observation: ", obs)
+            obs, reward, done, info = env.step(action)
+            print("Action: ", action)
+            print("Reward: ", reward)
+            print("Next observation: ", obs)
+            print()
+            cnt += 1
+            rewards.append(reward)
+        reward_list.append(rewards)
+        all_rewards.append(np.mean(reward_list))
+    env.close()
+    print("Average reward: ", np.mean(reward_list))
